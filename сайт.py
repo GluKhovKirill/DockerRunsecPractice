fb94245af20e548edb964ef644a56788739b0f37
Fedyok <45348624+Fedyok@users.noreply.github.com>
2024-05-04 00:04:37 +0300

    Add files via upload

--- сайт.py
+++ сайт.py
@@ -3,6 +3,7 @@ from flask import *
 import bd
 import sqlite3
 from flask import render_template
+import quires
 
 
 app = Flask(__name__)
@@ -12,10 +13,22 @@ PORT = 8080
 mybd = sqlite3.connect('users.db', check_same_thread=False)
 
 nic = ''
+age = 0
 
+def add_user(username: str, age: int) -> bool:
+    return True
 
-@app.route('/form_sample', methods=['POST', 'GET'])
-def form_sample():
+def get_user_age(username: str) -> int:
+    return 
+
+def create_db():
+    pass
+
+def create_table():
+    pass
+
+@app.route('/user', methods=['POST', 'GET'])
+def user():
     if request.method == 'GET':
         return '''<!doctype html>
                         <html lang="en">
@@ -30,54 +43,18 @@ def form_sample():
                             <title>Регистрация</title>
                           </head>
                           <body>
-                            <h1>Вы не зарегистрированы в системе КиноКрит!</h1>
+                            <h1>Вы не зарегистрированы в системе Бонча!</h1>
                             <form method="post">
                               <div class="form-group">
-                                <div class="form-group col-md-5">
-                                  <label for="inputEmail4">Email</label>
-                                  <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
-                                </div>
-                                <div class="form-group col-md-5">
-                                  <label for="inputPassword4">Password</label>
-                                  <input type="password" class="form-control" id="password" placeholder="Введите пароль" name="password">
-                                </div>
-                              </div>
                               <div class="form-group col-md-5">
-                                <label for="nic">Nic</label>
-                                <input type="text" class="form-control" id="nic" aria-describedby="emailHelp" placeholder="Введите Ваш ник" name="nic">
+                                <label for="nic">Имя</label>
+                                <input type="text" class="form-control" id="nic" aria-describedby="emailHelp" placeholder="Введите Ваше имя" name="nic">
                               </div>
                               <div class="form-group">
                                 <div class="form-group col-md-5">
-                                    <label for="about">Немного о себе</label>
-                                    <textarea class="form-control" id="about" rows="3" name="about"></textarea>
-                                </div>
-                                <div class="form-group col-md-4">
                                   <label for="classSelect">Возраст</label>
-                                  <select id="classSelect" class="form-control">
-                                    <option selected>Меньше 14</option>
-                                    <option>14</option>
-                                    <option>15</option>
-                                    <option>16</option>
-                                    <option>17</option>
-                                    <option>18</option>
-                                    <option>Больше 18</option>
-                                  </select>
+                                  <input type="int" class="form-control" id="age" aria-describedby="emailHelp" placeholder="Введите Ваш возраст" name="age">
                                 </div>
-                              </div>
-                              <div class="form-group col-md-5">
-                                    <label for="form-check">Укажите пол</label>
-                                    <div class="form-check">
-                                      <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
-                                      <label class="form-check-label" for="male">
-                                        Мужской
-                                      </label>
-                                    </div>
-                                    <div class="form-check">
-                                      <input class="form-check-input" type="radio" name="sex" id="female" value="female">
-                                      <label class="form-check-label" for="female">
-                                        Женский
-                                      </label>
-                                    </div>
                               </div>
                               <button type="submit" class="btn btn-outline-success col-md-2">Регистрация</button>
                             </form>
@@ -85,10 +62,20 @@ def form_sample():
                           </body>
                         </html>'''
     elif request.method == 'POST':
-        global nic
         nic = request.form['nic']
+        age = request.form['age']
+        quires.add_user(nic,  age)
+        print(nic, age)
+    #return redirect('/get_age')
+    return nic, age
 
-        user.insert(request.form['nic'], request.form['password'],
-                    request.form['class'], request.form['email'], request.form['sex'])
+@app.route('/get_age', methods=['GET'])
+def get_age():
+    user = request.args.get('nic')
+    uage = quires.get_user_age(user)
+    return str(uage)
 
-        return redirect('/profile/' + request.form['nic'])
+if __name__ == '__main__':
+    quires.create_db()
+    quires.create_table('main')
+    app.run(port=PORT, host=HOST)
